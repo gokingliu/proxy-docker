d28329392ea086b3c58a9b5cf8693f0e83d2e733
crotaliu <crotaliu@tencent.com>
2022-10-22 20:25:59 +0800

    feat: 增加请求鉴权

--- nginx/app/entrypoint.sh
+++ nginx/app/entrypoint.sh
@@ -8,6 +8,10 @@ NAMESERVER=$(awk '/^nameserver/{print $2}' ${RESOLVER_CONF})
 DEFAULT_NAMESERVER='127.0.0.11'
 # Nginx 配置文件
 NGINX_CONF=/etc/nginx/nginx.conf
+# 鉴权服务文件
+AUTH_FILE=/app/sign_check_nginx_linux_amd64
+# 鉴权 Token
+TOKEN='xxx'
 
 # 判断本地 DNS 配置文件是否存在
 if [ ! -e "${RESOLVER_CONF}" ]; then
@@ -23,6 +27,9 @@ fi
 if [ "$(grep -c "${DEFAULT_NAMESERVER}" "${NGINX_CONF}")" -ne '0' ]; then
   sed -i "s/127.0.0.11/${NAMESERVER}/" "${NGINX_CONF}"
 fi
+
+# 启动鉴权服务
+nohup ${AUTH_FILE} -bind '0.0.0.0:8287' -token ${TOKEN} > /dev/null 2>&1 &
 
 # nginx 守护进程
 nginx -g "daemon off;"
